466bc03dc0db1bc800f7c6294f0dfe0fb0868ae8
Maciej, Jedwabny <maciej.jedwabny@intel.com>
2022-12-30 01:38:48 +0100

    day12 part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -9,9 +9,24 @@ mod day12 {
         let current_coords = raw_grid.get_current_position_coords();
         let best_signal_coords = raw_grid.get_best_signal_position_coords();
 
-        let navigation = Navigation::new(digital_grid);
+        let mut navigation = Navigation::new(digital_grid);
+        navigation.analyze_routes(best_signal_coords);
 
-        return navigation.find_shortest_path_length(current_coords, best_signal_coords);
+        return navigation.find_shortest_path_length(&current_coords);
+    }
+
+    pub fn fewest_steps_required_to_reach_best_signal_location_from_any_square_at_elevation_a(input: &mut dyn BufRead) -> usize {
+        let raw_grid = parse_input(input);
+        let digital_grid = raw_grid.digitize();
+        let coords_at_elevation_a = digital_grid.find_coords_at_elevation(raw_grid.digitize_position('a'));
+        let best_signal_coords = raw_grid.get_best_signal_position_coords();
+
+        let mut navigation = Navigation::new(digital_grid);
+        navigation.analyze_routes(best_signal_coords);
+
+        return coords_at_elevation_a.iter()
+            .map(|c| navigation.find_shortest_path_length(c))
+            .min().unwrap_or(usize::MAX);
     }
 
     fn parse_input(input: &mut dyn BufRead) -> Grid<char> {
@@ -75,14 +90,14 @@ mod day12 {
         }
 
         fn digitize_line(&self, line: &Vec<char>) -> Vec<usize> {
-            return line.iter().map(|p| self.digitize_position(p)).collect();
+            return line.iter().map(|p| self.digitize_position(*p)).collect();
         }
 
-        fn digitize_position(&self, position: &char) -> usize {
+        fn digitize_position(&self, position: char) -> usize {
             let unified_position = match position {
                 'S' => 'a',
                 'E' => 'z',
-                _ => *position
+                _ => position
             };
             return unified_position as usize - 'a' as usize;
         }
@@ -92,34 +107,42 @@ mod day12 {
         fn get_neighbours(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
             return vec![
                 if x > 0 { Some((x - 1, y)) } else { None },
-                if y < self.height - 1 {Some((x, y + 1))} else {None},
-                if x < self.width -1 {Some((x + 1, y))} else {None},
-                if y > 0 {Some((x, y - 1))} else {None}
+                if y < self.height - 1 { Some((x, y + 1)) } else { None },
+                if x < self.width - 1 { Some((x + 1, y)) } else { None },
+                if y > 0 { Some((x, y - 1)) } else { None }
             ].iter().filter(|c| c.is_some()).map(|c| c.unwrap()).collect();
+        }
+
+        fn find_coords_at_elevation(&self, elevation: usize) -> Vec<(usize, usize)> {
+            return self.get_coords().iter()
+                .map(|(x, y)| ((x, y), self.get_position(*x, *y)))
+                .filter(|(_, e)| **e == elevation)
+                .map(|((x, y), _)| (*x, *y))
+                .collect();
         }
     }
 
     struct Navigation {
         grid: Grid<usize>,
-        graph: HashMap<(usize, usize), HashSet<(usize, usize)>>
+        graph: HashMap<(usize, usize), HashSet<(usize, usize)>>,
+        distances: HashMap<(usize, usize), usize>
     }
 
     impl Navigation {
         fn new(grid: Grid<usize>) -> Navigation {
-            return Navigation { graph: GraphBuilder::new(&grid).build(), grid };
+            return Navigation { graph: GraphBuilder::new(&grid).build(), grid, distances: HashMap::new() };
         }
 
-        fn find_shortest_path_length(&self, from: (usize, usize), to: (usize, usize)) -> usize {
-            let mut distances: HashMap<(usize, usize), usize> = HashMap::new();
+        fn analyze_routes(&mut self, to: (usize, usize)) {
             let mut predecessors: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
             let mut to_check = self.grid.get_coords().iter().map(|v| *v).collect::<HashSet<(usize, usize)>>();
 
-            distances.insert(to, 0);
+            self.distances.insert(to, 0);
 
             for _ in 0..to_check.len() {
                 let mut nodes: Vec<((usize, usize), &usize)> = to_check.iter()
-                    .filter(|q| distances.contains_key(q))
-                    .map(|q| (*q, distances.get(q).unwrap()))
+                    .filter(|q| self.distances.contains_key(q))
+                    .map(|q| (*q, self.distances.get(q).unwrap()))
                     .collect();
                 nodes.sort_by(|a, b| a.1.cmp(b.1));
                 
@@ -127,17 +150,19 @@ mod day12 {
                     to_check.remove(&nearest_node);
 
                     for neighbour in self.graph.get(&nearest_node).unwrap() {
-                        let existing_distance = distances.get(neighbour);
-                        let candidate_distance = *distances.get(&nearest_node).unwrap();
+                        let existing_distance = self.distances.get(neighbour);
+                        let candidate_distance = *self.distances.get(&nearest_node).unwrap();
                         if existing_distance.map(|d| *d > candidate_distance + EDGE_COST).unwrap_or(true) {
-                            distances.insert(*neighbour, candidate_distance + EDGE_COST);
+                            self.distances.insert(*neighbour, candidate_distance + EDGE_COST);
                             predecessors.insert(*neighbour, nearest_node);
                         }
                     }
                 }
             }
-
-            return *distances.get(&from).unwrap();
+        }
+        
+        fn find_shortest_path_length(&self, from: &(usize, usize)) -> usize {
+            return self.distances.get(from).map(|d| *d).unwrap_or(usize::MAX);
         }
     }
 
@@ -195,5 +220,17 @@ mod tests {
     fn part1_day_input() {
         let mut f = BufReader::new(File::open("./day.input").unwrap());
         assert_eq!(day12::fewest_steps_required_to_reach_best_signal_location_from_current_position(&mut f), 423);
+    }
+
+    #[test]
+    fn part2_sample_input() {
+        let mut f = BufReader::new(File::open("./sample.input").unwrap());
+        assert_eq!(day12::fewest_steps_required_to_reach_best_signal_location_from_any_square_at_elevation_a(&mut f), 29);
+    }
+
+    #[test]
+    fn part2_day_input() {
+        let mut f = BufReader::new(File::open("./day.input").unwrap());
+        assert_eq!(day12::fewest_steps_required_to_reach_best_signal_location_from_any_square_at_elevation_a(&mut f), 416);
     }
 }
